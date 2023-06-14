0cdc0d8b4307fb7d7c09ec387f017bf683765e5c
Rafsanjani N.I <45141323+maplezs@users.noreply.github.com>
2023-06-15 02:47:40 +0700

    Update recommender.py

--- modules/recommender.py
+++ modules/recommender.py
@@ -47,9 +47,9 @@ def distance_from(loc1, loc2):
 def connect_database():
     try:
         mydb = mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
+            host=os.environ.get("DB_HOST"),
             user="root",
-            password=os.getenv("DB_PASS"),
+            password=os.environ.get("DB_PASS"),
             database="hayuk_ml"
         )
         return mydb
